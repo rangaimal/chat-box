84976c29afd20dff618c156f49e4638073fbeeab
rangaimal <rangaimal1234@gmail.com>
2018-05-27 23:49:57 +0530

    add new user

---
+++ newuser.php
@@ -0,0 +1,25 @@
+ <link rel="stylesheet" type="text/css" href="css/style.css">   
+                    <form action="signup.php" method="post">
+                   <body>
+                    <div id="main" class="main">
+                    <table>
+                    <tr>
+                    <td><label style="color: white"><p>User Name:</p></label></td>
+                    <td><input type="text" name="uname" placeholder="UserName"></td>
+                    </tr>
+
+                    <tr>
+                    <td><label style="color: white"><p>Email:</p></label></td>
+                    <td><input type="text" name="Email" placeholder="Email"></td>
+                    </tr>
+
+                    <tr>
+                    <td><label style="color:white"><p>Password:</p></label></td>
+                    <td><input type="text" name="Password" placeholder="Password"></td>
+                    </tr>
+                    </table>
+                    
+                    <button 
+                    type="submit" name="submit"><b>signup</b></button>
+                    </form>
+ </body>
